93a5f7bfbd7ac4ffd19a5166ac6cb902df5d18e0
lgmacedo <lucasguerra.macedo@gmail.com>
2023-01-18 14:27:39 -0300

    tela de entrada

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-/* ============== USUÁRIO ================================================================================= */
+/* ============== VARIÁVEIS GLOBAIS ================================================================================= */
 let nomeUsuario;
 let destinatario = "Todos";
 let tipoMsg = "message";
 
+/* ============== USUÁRIO ================================================================================= */
 function tratarSucessoUsuario(dados){
     console.log("Deu certo!!!");
     console.log(dados);
     setInterval(mantemOnline, 5000);
+    document.querySelector('.entrada').classList.add('escondido');
+    document.querySelector('header').classList.remove('escondido');
+    document.querySelector('main').classList.remove('escondido');
+    document.querySelector('footer').classList.remove('escondido');
+    buscaMensagens();
+    setInterval(buscaMensagens, 3000);
+    document.querySelector('footer input').style.setProperty("--c", "black");
+    document.querySelector('footer input').style.setProperty("--f", "italic");
 }
 
 function mantemOnline(){
@@ -17,11 +26,10 @@ function mantemOnline(){
 function tratarErroUsuario(dados){
     console.log("Deu errado!!!");
     console.log(dados);
-    perguntaNome();
 }
 
 function perguntaNome(){
-    nomeUsuario = prompt("Insira o seu nome");
+    nomeUsuario = document.querySelector('.entrada input').value;
     const Usuario = {name:nomeUsuario};
     const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", Usuario);
     promessa.then(tratarSucessoUsuario);
@@ -62,7 +70,7 @@ function tratarErroMensagens(dados){
 }
 
 function mensagemEnviada(){
-    document.querySelector('input').value = "";
+    document.querySelector('footer input').value = "";
     buscaMensagens();
 }
 
@@ -71,7 +79,7 @@ function mensagemNãoEnviada(){
 }
 
 function enviaMensagem(){
-    const mensagem = document.querySelector('input').value;
+    const mensagem = document.querySelector('footer input').value;
     const msgObj = { 
         from: nomeUsuario,
         to: destinatario,
@@ -81,16 +89,20 @@ function enviaMensagem(){
     const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", msgObj);
     promessa.then(mensagemEnviada);
     promessa.catch(mensagemNãoEnviada);
+
 }
 
 /* ============== SCRIPT DE FATO ================================================================= */
-perguntaNome();
-buscaMensagens();
-setInterval(buscaMensagens, 3000);
-
+// configuração do Inserir Nome com Enter
+let input1 = document.querySelector('.entrada input');
+input1.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    perguntaNome();
+  }
+});
 // configuração do Enviar Mensagem com Enter
-let input = document.querySelector('input');
-input.addEventListener("keypress", function(event) {
+let input2 = document.querySelector('footer input');
+input2.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     enviaMensagem();
   }
