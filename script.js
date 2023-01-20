a8ba6ab68b7451a3f46e45437c7b4ebca6ad01b5
lgmacedo <lucasguerra.macedo@gmail.com>
2023-01-20 10:43:30 -0300

    refatoração inicial e ajustes finos

--- script.js
+++ script.js
@@ -2,21 +2,22 @@
 let nomeUsuario;
 let destinatario = "Todos";
 let tipoMsg = "message";
-let participantesOnline = [];
+
+const cincoSegundos = 5000;
+const tresSegundos = 3000;
+const dezSegundos = 10000;
 
 /* ============== USUÁRIOS ================================================================================= */
 function tratarSucessoUsuario(dados){
-    console.log("Deu certo!!!");
-    console.log(dados);
-    setInterval(mantemOnline, 5000);
+    setInterval(mantemOnline, cincoSegundos);
     document.querySelector('.entrada').remove();
     document.querySelector('header').classList.remove('escondido');
     document.querySelector('main').classList.remove('escondido');
     document.querySelector('footer').classList.remove('escondido');
     buscaMensagens();
-    setInterval(buscaMensagens, 3000);
+    setInterval(buscaMensagens, tresSegundos);
     buscaParticipantes();
-    setInterval(buscaParticipantes, 10000);
+    setInterval(buscaParticipantes, dezSegundos);
     document.querySelector('footer input').style.setProperty("--c", "black");
     document.querySelector('footer input').style.setProperty("--f", "italic");
 }
@@ -30,9 +31,14 @@ function tratarSucessoParticipantes(recebido){
     const participante = recebido.data;
     document.querySelector('.participantesOnline').innerHTML = "";
     for(let i = 0; i<participante.length; i++){
-        document.querySelector('.participantesOnline').innerHTML += `<div data-test = "participant" onclick="marcaDestinatario(this)" class="interiorParticipante"><ion-icon name="person-circle-sharp"></ion-icon>
-        <p>${participante[i].name}</p><div class="checkVerdeDestinatario escondido">
-        <ion-icon data-test = "check" name="checkmark-sharp"></ion-icon></div></div>`
+        document.querySelector('.participantesOnline').innerHTML += 
+        `<div data-test = "participant" onclick="marcaDestinatario(this)" class="interiorParticipante">
+            <ion-icon name="person-circle-sharp"></ion-icon>
+            <p>${participante[i].name}</p>
+            <div class="checkVerdeDestinatario escondido">
+                <ion-icon data-test = "check" name="checkmark-sharp"></ion-icon>
+            </div>
+        </div>`
     }
 }
 
@@ -42,12 +48,11 @@ function mantemOnline(){
 }
 
 function tratarErroUsuario(dados){
-    console.log("Deu errado!!!");
-    console.log(dados);
     document.querySelector('.carregando').classList.add('escondido');
     document.querySelector('.entrada input').classList.remove('escondido');
     document.querySelector('.entrada button').classList.remove('escondido');
-    if(dados.response.status===400){
+    const numErroUsuarioJaLogado = 400;
+    if(dados.response.status===numErroUsuarioJaLogado){
         alert("Usuário já logado. Insira outro nome");
     }else{
         alert("Erro. Tente novamente");
@@ -70,33 +75,42 @@ function perguntaNome(){
 function buscaMensagens(){
     const promessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     promessa.then(tratarSucessoMensagens);
-    promessa.catch(tratarErroMensagens);
 }
 
 function tratarSucessoMensagens(recebido){
-    console.log("Deu certo!!!");
-    console.log(recebido);
     const mensagem = recebido.data;
     const innerAnterior = document.querySelector('main').innerHTML;
     document.querySelector('main').innerHTML = "";
     for(let i=0; i<mensagem.length; i++){
         if(mensagem[i].type === "message"){
-            document.querySelector('main').innerHTML += `<div data-test="message"><span class="fontColorGrey">(${mensagem[i].time})</span>&nbsp;<span class="font700">${mensagem[i].from}</span>&nbsp;para&nbsp;<span class="font700">${mensagem[i].to}</span>: ${mensagem[i].text}</div>`
+            document.querySelector('main').innerHTML += 
+            `<div data-test="message">
+                <span class="fontColorGrey">(${mensagem[i].time})</span>&nbsp;
+                <span class="font700">${mensagem[i].from}</span>
+                para
+                <span class="font700">${mensagem[i].to}</span>: ${mensagem[i].text}
+            </div>`
         }else if(mensagem[i].type === "status"){
-            document.querySelector('main').innerHTML += `<div data-test="message" class="bgCinza"><span class="fontColorGrey">(${mensagem[i].time})</span>&nbsp;<span class="font700">${mensagem[i].from}</span>&nbsp;${mensagem[i].text}</div>`
+            document.querySelector('main').innerHTML += 
+            `<div data-test="message" class="bgCinza">
+                <span class="fontColorGrey">(${mensagem[i].time})</span>&nbsp;
+                <span class="font700">${mensagem[i].from}</span>
+                ${mensagem[i].text}
+            </div>`
         }else if(mensagem[i].type === "private_message" && (mensagem[i].to === nomeUsuario || mensagem[i].from === nomeUsuario)){
-            document.querySelector('main').innerHTML += `<div data-test="message" class="bgVermelho"><span class="fontColorGrey">(${mensagem[i].time})</span>&nbsp;<span class="font700">${mensagem[i].from}</span>&nbsp;reservadamente para&nbsp;<span class="font700">${mensagem[i].to}</span>: ${mensagem[i].text} </div>`
+            document.querySelector('main').innerHTML += 
+            `<div data-test="message" class="bgVermelho">
+                <span class="fontColorGrey">(${mensagem[i].time})</span>&nbsp;
+                <span class="font700">${mensagem[i].from}</span>
+                reservadamente para
+                <span class="font700">${mensagem[i].to}</span>: ${mensagem[i].text} 
+            </div>`
         }
     }
     document.querySelector('main').innerHTML += `<aside><aside>`;
     if(document.querySelector('main').innerHTML !== innerAnterior){
         document.querySelector('aside').scrollIntoView();
     }
-}
-
-function tratarErroMensagens(dados){
-    console.log("Deu errado!!!");
-    console.log(dados);
 }
 
 function mensagemEnviada(){
@@ -119,7 +133,6 @@ function enviaMensagem(){
     const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", msgObj);
     promessa.then(mensagemEnviada);
     promessa.catch(mensagemNãoEnviada);
-
 }
 
 /* ============== PARTICIPANTES ATIVOS ================================================================= */
@@ -134,17 +147,20 @@ function fechaParticipantes(){
 }
 
 function marcaVisibilidade(qual){
-    if(!(document.querySelector('.publico .checkVerdeVisibilidade').classList.contains('escondido')));
-        document.querySelector('.publico .checkVerdeVisibilidade').classList.add('escondido');
-    if(!(document.querySelector('.reservadamente .checkVerdeVisibilidade').classList.contains('escondido')));
-        document.querySelector('.reservadamente .checkVerdeVisibilidade').classList.add('escondido');
+    const checkPublico = document.querySelector('.publico .checkVerdeVisibilidade');
+    if(!(checkPublico.classList.contains('escondido')));
+        checkPublico.classList.add('escondido');
+    const checkReservado = document.querySelector('.reservadamente .checkVerdeVisibilidade');
+    if(!(checkReservado.classList.contains('escondido')));
+        checkReservado.classList.add('escondido');
     qual.querySelector('.checkVerdeVisibilidade').classList.remove('escondido');
+    const mensagemInf = document.querySelector('.textoInf').innerHTML;
     if(qual.classList.contains('reservadamente')){
         tipoMsg = "private_message";
-        document.querySelector('.textoInf').innerHTML = `Enviando para ${destinatario} (reservadamente)`
+        mensagemInf = `Enviando para ${destinatario} (reservadamente)`
     }else{
         tipoMsg = "message"
-        document.querySelector('.textoInf').innerHTML = `Enviando para ${destinatario}`
+        mensagemInf = `Enviando para ${destinatario}`
     }
 }
 
@@ -155,23 +171,23 @@ function marcaDestinatario(qual){
     }
     qual.querySelector('.checkVerdeDestinatario').classList.remove('escondido');
     destinatario = qual.querySelector('p').innerHTML;
+    const mensagemInf = document.querySelector('.textoInf').innerHTML;
     if(tipoMsg === "message")
-        document.querySelector('.textoInf').innerHTML = `Enviando para ${destinatario}`
+        mensagemInf = `Enviando para ${destinatario}`
     if(tipoMsg === "private_message")
-        document.querySelector('.textoInf').innerHTML = `Enviando para ${destinatario} (reservadamente)`
+        mensagemInf = `Enviando para ${destinatario} (reservadamente)`
 }
 
 /* ============== SCRIPT DE FATO ================================================================= */
 // configuração do Inserir Nome com Enter
-let input1 = document.querySelector('.entrada input');
-input1.addEventListener("keypress", function(event) {
+document.querySelector('.entrada input').addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     perguntaNome();
   }
 });
+
 // configuração do Enviar Mensagem com Enter
-let input2 = document.querySelector('footer input');
-input2.addEventListener("keypress", function(event) {
+document.querySelector('footer input').addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     enviaMensagem();
   }
